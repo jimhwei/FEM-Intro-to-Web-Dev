1fed0a7443e893b0f5f72caa4c0e7c523a13446b
wemjii <60511633+wemjii@users.noreply.github.com>
2021-01-12 15:11:22 -0500

    putting doggos on the web page using api

--- API Demo/doggos.js
+++ API Demo/doggos.js
@@ -1,12 +1,34 @@
+// const DOG_URL = "https://dog.ceo/api/breeds/image/random";
+// const promise = fetch(DOG_URL);
+
+// promise.then(function(response) {
+//   const processingPromise = response.json();
+//   return processingPromise;
+// })
+//   .then(function(processedResponse) {
+//     console.log(processedResponse);
+//   })
+
+// console.log("This will log first");
+
+
+// Promise Chaining
 const DOG_URL = "https://dog.ceo/api/breeds/image/random";
+
 const promise = fetch(DOG_URL);
+const doggos = document.querySelector(".doggos");
 
-promise.then(function(response) {
-  const processingPromise = response.json();
-  return processingPromise;
-})
-  .then(function(processedResponse) {
-    console.log(processedResponse);
+promise
+  .then(function(response) {
+    const processingPromise = response.json();
+    return processingPromise;
   })
+  .then(function(processedResponse) {
+    const img = document.createElement("img");
+    img.src = processedResponse.message;
+    img.alt = "Cute doggo";
+    doggos.appendChild(img);
+  });
 
-console.log("This will log first");
+
+document.querySelector(".add-doggo").addEventListener("click", addNewDoggo);
